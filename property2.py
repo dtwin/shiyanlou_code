79b941fa571fcbeb06daf2449a6a3ae6900746af
dtwin <495271301@qq.com>
2019-08-17 01:58:44 +0800

    property

---
+++ property2.py
@@ -0,0 +1,56 @@
+class Account(object):
+
+    """???,
+
+    amount ?????.
+
+    """
+
+    def __init__(self, rate):
+
+        self.__amt = 0
+
+        self.rate = rate
+
+
+
+
+    @property
+
+    def amount(self):                                                               return self.__amt
+
+
+
+    @property
+
+    def cny(self):
+
+        return self.__amt * self.rate
+
+
+    @amount.setter
+
+    def amount(self, value):
+
+        if value<0
+
+        print("Sorry, no negative amount in the account.")
+
+        return
+
+    self.__amt = value
+
+if __name__ == '__main__':
+
+
+    acc = Account(rate=6.6) # ??????????
+
+    acc.amount = 20
+
+    print("Dollar amount:", acc.amount)
+
+    print("In CNY:", acc.cny)
+
+    acc.amount = -100
+
+    print("Dollar amount:", acc.amount)
